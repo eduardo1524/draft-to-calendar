8398496f535db6b700a05eb0ddc4dd2768f5c46f
adrianes02 <105319194+adrianes02@users.noreply.github.com>
2022-05-16 17:43:59 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -2,4 +2,4 @@ api_hash = "apihash"
 api_id = 1736372819
 bot_token = "5217944853:AAFvIrOyOjCTOZthlYRsh68sFDxb_ObV2i8"
 authorized_users = [1315636728]
-admin_user = '@LAES2002'
+admin_user = 'LAES2002'
