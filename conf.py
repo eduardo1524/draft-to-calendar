ddbf2b77e636e6f0b3701c413da69bd247aec403
adrianes02 <105319194+adrianes02@users.noreply.github.com>
2022-05-16 17:33:03 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
 api_hash = "apihash"
 api_id = 1736372819
-bot_token = "bottoken"
+bot_token = "5217944853:AAFvIrOyOjCTOZthlYRsh68sFDxb_ObV2i8"
 authorized_users = []
-admin_user = 'admindelbot'
+admin_user = '1315636728'
