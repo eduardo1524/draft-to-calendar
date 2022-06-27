aca1963511dc2e422fef52ac22fd222ff6720ab5
eduardo1524 <103707138+eduardo1524@users.noreply.github.com>
2022-06-27 10:37:16 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
 api_hash = "cdbea1fb2a335600740eb67a465b3084"
 api_id = 12875919
-bot_token = "5217944853:AAEe4eAoxc_fhYBkgd1V9lgbXIw9iPRV5Mc"
+bot_token = "5246945068:AAFXrB2vChsfBa0aCBvJMQFdXJZ1p9tUiZM"
 authorized_users = [1315636728]
-admin_user = 'LAES2002'
+admin_user = 'Eduardo_1524'
